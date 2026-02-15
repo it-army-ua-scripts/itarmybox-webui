45040006a7337bd86fa23fea5e243ae22b70a27e
OleksandrBlack <aleksandr.chornuy@gmail.com>
2026-02-15 22:38:06 +0200

    fix update & logo

--- update.sh
+++ update.sh
@@ -2,5 +2,7 @@
 
 echo "START UPDATE"
 cd /var/www/html/itarmybox-webui/
-/usr/bin/git checkout main -f && git pull
+/usr/bin/git fetch origin main
+/usr/bin/git reset --hard origin/main
+/usr/bin/git clean -fd
 echo "DONE!"
